208f27e7b87c5ecb5f6a65accd78bedc76ab5842
thiago braz <thiagoadsbraz@gmail.com>
2024-11-07 16:06:44 -0400

    Primeiro Projeto1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int valor1 = 0;
+int valor2 = 0;
+int valor3 = 0;
+
+Console.WriteLine("Digite um valor");
+valor1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor");
+valor2 = int.Parse(Console.ReadLine());
+
+valor3 = valor1 + valor2;
+Console.WriteLine("O valor e !"+ valor3);
