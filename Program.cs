c0a53a6cc62936a0fcdb5b9160018ad71db8c5c8
Thiago Braz <thiagoadsbraz@gmail.com>
2024-11-08 14:26:11 -0400

    Projeto1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿int valor1 = 0;
 int valor2 = 0;
-int valor3 = 0;
+int resultado = 0;
 
 Console.WriteLine("Digite um valor");
 valor1 = int.Parse(Console.ReadLine());
@@ -8,5 +8,16 @@ valor1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Digite um valor");
 valor2 = int.Parse(Console.ReadLine());
 
-valor3 = valor1 + valor2;
-Console.WriteLine("O valor e !"+ valor3);
+resultado = valor1 + valor2;
+
+// como fazer if e else
+if (resultado > 2)
+{
+    
+    Console.WriteLine("Seu resultado e positivo");
+
+}
+else 
+{
+    Console.WriteLine("Seu resultado e negativo");
+}
